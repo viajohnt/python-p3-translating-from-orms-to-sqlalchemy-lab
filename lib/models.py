748b3fadc32b555afcbfdce0ef31a4d09e4f46ef
John Truong <viajohnt@gmail.com>
2023-05-07 16:49:50 -0600

    Final Commit

--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import (Column, String, Integer)
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 
 Base = declarative_base()
 
